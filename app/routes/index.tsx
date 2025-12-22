b893eac418a12e6faf82a5b212cafac2d5f68cb6
liujip0 <liujip2020@gmail.com>
2025-12-22 02:29:44 -0500

    finish landing page for logged out user

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@liujip0/components";
+import { Link } from "react-router";
 import { userContext } from "~/context/context.js";
+import commonStyles from "../common.module.css";
 import type { Route } from "./+types/index";
+import styles from "./index.module.css";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -15,5 +19,37 @@ export function loader({ context }: Route.LoaderArgs) {
 }
 
 export default function Index({ loaderData }: Route.ComponentProps) {
-  return <div>{loaderData.user ? <></> : <></>}</div>;
+  return (
+    <div className={styles.page}>
+      {loaderData.user ? (
+        <></>
+      ) : (
+        <>
+          <h1 className={styles.title}>Welcome to Latin Vocab!</h1>
+          <p className={styles.text}>Please log in or create an account.</p>
+          <div className={styles.signinButtons}>
+            <Link
+              to="/login"
+              className={commonStyles.noUnderline}>
+              <Button>Log In</Button>
+            </Link>
+            <Link
+              to="/signup"
+              className={commonStyles.noUnderline}>
+              <Button>Sign Up</Button>
+            </Link>
+          </div>
+          <p className={styles.text}>
+            All code for this project is open-source on{" "}
+            <a
+              href="https://github.com/liujip0/latin-vocab"
+              target="_blank">
+              GitHub
+            </a>
+            .
+          </p>
+        </>
+      )}
+    </div>
+  );
 }
--- app/routes/topbar.tsx
+++ app/routes/topbar.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@liujip0/components";
 import { Link, Outlet } from "react-router";
 import { userContext } from "~/context/context.js";
+import commonStyles from "../common.module.css";
 import type { Route } from "./+types/topbar";
 import styles from "./topbar.module.css";
 
@@ -16,7 +17,7 @@ export default function TopBar({ loaderData }: Route.ComponentProps) {
       <div className={styles.topbar}>
         <Link
           to="/"
-          className={styles.noLinkUnderline}>
+          className={commonStyles.noUnderline}>
           <h1 className={styles.title}>Latin Vocab</h1>
         </Link>
         {loaderData.user ? (
@@ -25,12 +26,12 @@ export default function TopBar({ loaderData }: Route.ComponentProps) {
           <div className={styles.topbarButtons}>
             <Link
               to="/login"
-              className={styles.noLinkUnderline}>
+              className={commonStyles.noUnderline}>
               <Button className={styles.topbarButton}>Log In</Button>
             </Link>
             <Link
               to="/signup"
-              className={styles.noLinkUnderline}>
+              className={commonStyles.noUnderline}>
               <Button className={styles.topbarButton}>Sign Up</Button>
             </Link>
           </div>
